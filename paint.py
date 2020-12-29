d836980a149a5323b2fbdc41fdb9c94cd214bffc
Gyu-Hyeon Park <rudals1946@naver.com>
2020-12-29 10:49:54 +0900

    02

--- paint.py
+++ paint.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QHBoxLayout, QWidget
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QHBoxLayout, QWidget, QPushButton
 from PyQt5.QtGui import QPainter, QPen, QBrush, QPixmap, QColor
 from PyQt5.QtCore import Qt
 
@@ -6,13 +7,15 @@ class Canvas(QLabel):
     def __init__(self, size):
         super().__init__()
         
+        self.initBtn()
+
         self.size = size
         self.setPixmap(QPixmap(*size))
         self.setFixedSize(*size)
-        
+
         # 그림판 초기화
         self.clear_canvas()
-    
+
     # 그림 그릴 곳을 하얀색으로 칠하기
     def clear_canvas(self):
         painter = QPainter(self.pixmap())
@@ -20,6 +23,28 @@ class Canvas(QLabel):
         painter.drawRect(-100, -100, self.size[0]+100, self.size[1]+100)
         painter.end()
     
+    def initBtn(self):
+        btnPen = QPushButton('pen', self)	# 버튼 텍스트
+        btnPen.resize(50, 50)
+        btnPen.move(20, 20)	# 버튼 위치
+        #btnPen.clicked.connect(self.mouseMoveEvent())
+
+        btnSquare = QPushButton('사각형', self)	# 버튼 텍스트
+        btnSquare.resize(50, 50)
+        btnSquare.move(80, 20)	# 버튼 위치
+        #btnSquare.clicked.connect()
+
+        btnTriangle = QPushButton('세모', self)	# 버튼 텍스트
+        btnTriangle.resize(50, 50)
+        btnTriangle.move(140, 20)	# 버튼 위치
+        #btnTriangle.clicked.connect()
+
+        btnCircle = QPushButton('원', self)	# 버튼 텍스트
+        btnCircle.resize(50, 50)
+        btnCircle.move(200, 20)	# 버튼 위치
+        #btnCircle.clicked.connect()
+
+class Pen(QLabel):
     # 마우스가 눌린 상태에서 움직이면 발생
     def mouseMoveEvent(self, e):
         painter = QPainter(self.pixmap())
@@ -36,7 +61,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.Canvas = Canvas((1250, 700))
+        self.Canvas = Canvas((1260, 700))
         self.setFixedSize(1280, 720)
 
         MainWidget = QWidget()
@@ -46,7 +71,6 @@ class MainWindow(QMainWindow):
         
         self.setCentralWidget(MainWidget)
         self.show()
-
 
 if __name__ == "__main__":
     app = QApplication([])
